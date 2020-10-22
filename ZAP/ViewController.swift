182718bc10338ff115af7e487de65e8dadea0128
Zachary Perlmutter <zach.permlutter@gmail.com>
2020-10-21 23:35:15 -0400

    Create hello world alert button

--- ZAP/ViewController.swift
+++ ZAP/ViewController.swift
@@ -13,6 +13,13 @@ class ViewController: UIViewController {
     super.viewDidLoad()
     // Do any additional setup after loading the view.
   }
+  
+  // function (or method) to show an alert when button is pressed
+  @IBAction func showMessage(sender: UIButton) {
+    let alertController = UIAlertController(title: "Welcome to My First App", message: "Hello World", preferredStyle: UIAlertController.Style.alert)
+    alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+    present(alertController, animated: true, completion: nil)
+  }
 
 
 }
