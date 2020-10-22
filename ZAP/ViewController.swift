b0c78d4998a2326fe815aecf1d446693fcb784eb
Zachary Perlmutter <zach.permlutter@gmail.com>
2020-10-22 01:15:52 -0400

    Change meaning to button alerts

--- ZAP/ViewController.swift
+++ ZAP/ViewController.swift
@@ -16,7 +16,25 @@ class ViewController: UIViewController {
   
   // function (or method) to show an alert when button is pressed
   @IBAction func showMessage(sender: UIButton) {
-    let alertController = UIAlertController(title: "Welcome to My First App", message: "Hello World", preferredStyle: UIAlertController.Style.alert)
+    
+    // Initialize the dictionary for the emoji icons
+    let emojiDict: [String: String] = ["🔥": "Fire", "🤙🏾": "Gnarly", "☠️": "Dead", "❤️": "Love"]
+    var meaning = emojiDict["🔥"]
+    // var wordToLookUp = "🔥"
+    // var meaning = emojiDict[wordToLookUp]
+    
+    // The sender button is tapped by the user
+    // Here we store the sender button in the selectedButton constant
+    let selectedButton = sender
+    
+    // Get the emoji from the title label of the selected button
+    if let wordToLookUp = selectedButton.titleLabel?.text {
+      meaning = emojiDict[wordToLookUp]
+    }
+    
+    // Now instead of message: "Hello World", it is replaced with var meaning
+    
+    let alertController = UIAlertController(title: "Meaning", message: meaning, preferredStyle: UIAlertController.Style.alert)
     alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
     present(alertController, animated: true, completion: nil)
   }
